e082182e9aee45a0ccdd5ec440cd6a0dc713eff5
divyasondagar4 <divyasondagar4@gmail.com>
2024-07-15 16:43:27 +0530

    change

---
+++ php programs/multidearry.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$balances = array(
+    array(4.2, 6.4, 1.6, 6.8, 1.0),
+    array(0.5, 2.3, 4.1, 6.7, 8.9),
+    array(2.4, 4.5, 6.7, 8.1, 9.2)
+);
+
+echo "Element at row 0, column 2 is: " . $balances[0][2] ."<br>"; 
+echo "Element at row 1, column 4 is: " . $balances[1][4]."<br>"; 
+echo "Element at row 2, column 2 is: " . $balances[2][2]."<br>"; 
+
+?>
+
+</body>
+</html>
