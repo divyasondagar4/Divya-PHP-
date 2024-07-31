991a98a89d355826c35e121f6c8410f28239b342
divyasondagar4 <divyasondagar4@gmail.com>
2024-07-31 22:31:55 +0530

    change

--- Online-Admission-System-Project-In-PHP-MySQL-Source-Code/chmscIT2/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
- $db = mysqli_connect("localhost","root","","chmscit");
-
- if(mysqli_connect_errno())
- {
- 	echo "failed to connect to MySQL:".mysqli_connect_error();
- }
-?>
---
+++ project/food-track-admin/Train/header.php
@@ -0,0 +1,121 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet" />    
+ 
+<div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href=""><i class="fa fa-train" aria-hidden="true"></i>   Food-Track</a>
+            </div>
+            <div style="color: black;
+padding: 15px 50px 5px 50px;
+float: right;
+font-size: 16px;"> Last access :  &nbsp; <a href="#" class="btn btn-danger square-btn-adjust">Logout</a>
+            </div>
+        </nav>
+
+<nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li class="text-center">
+                        <img src="assets/img/find_user.png" class="user-image img-responsive" />
+                    </li>
+                    <li class="text-center" style="color:black; background-color:white; margin:10px;">
+                        <h4>Divya Sondagar </h>
+                    </li>
+
+                    <li>
+                        <a class="active-menu" href="index.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa-solid fa-shop fa-3x"></i>Restaurant</a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="add_Restaurant.php">Add Restaurant</a>
+                            </li>
+                            <li>
+                                <a href="manage_Restaurant.php">Manage Restaurant</a>
+                            </li>
+                        </ul> 
+                    </li>
+                    <li>
+                       <a href="#"><i class="fa-solid fa-utensils fa-3x"></i> Food</a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="add_food.php">Add Food</a>
+                            </li>
+                            <li>
+                                <a href="manage_food.php">Manage Food</a>
+                            </li>
+                        </ul> 
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-brands fa-first-order-alt fa-3x"></i>Order </a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="manage_order.php">Manage Order</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-desktop fa-3x"></i> Employee</a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="add_employee.php">Add Employee</a>
+                            </li>
+                            <li>
+                                <a href="manage_employee.php">Manage Employee</a>
+                            </li>
+                        </ul> 
+                    </li>
+                   
+                    <li>
+                        <a href="#"><i class="fa-solid fa-user fa-3x"></i>Customer </a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="manage_customer.php">Manage Customer</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-solid fa-address-book fa-3x"></i>Contact </a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="manage_contact.php">Manage Contact</a>
+                            </li>
+                        </ul>
+                    </li> 
+                    <li>
+                        <a href="#"><i class="fa-solid fa-cart-shopping fa-3x"></i>Cart </a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="manage_cart.php">Manage Cart</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-regular fa-comment fa-3x"></i>Feedback </a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="manage_feedback.php">Manage Feedback</a>
+                            </li>
+                        </ul>
+                    </li> 
+                 
+                    <li>
+                        <a href="#"><i class="fa-solid fa-city fa-3x"></i>City </a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="manage_city.php">Manage City</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
