75003f4877f7c60ac1d02a30ba37c1323a088819
divyasondagar4 <divyasondagar4@gmail.com>
2024-09-04 23:18:07 +0530

    change

---
+++ php programs/swappingm1.php
@@ -0,0 +1,20 @@
+<?php
+// Initial values
+$a = 5;
+$b = 10;
+
+// Display original values
+echo "Before swapping:<br>";
+echo "a = $a<br>";
+echo "b = $b<br>";
+
+// Swap the numbers
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+// Display swapped values
+echo "After swapping:<br>";
+echo "a = $a<br>";
+echo "b = $b<br>";
+?>
