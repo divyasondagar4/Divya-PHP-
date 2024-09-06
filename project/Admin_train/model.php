6402643f70865018f78d40dad8f64e13a862dc68
divyasondagar4 <divyasondagar4@gmail.com>
2024-09-06 18:52:22 +0530

    change

--- project/Admin_train/model.php
+++ project/Admin_train/model.php
@@ -20,6 +20,21 @@ class model
 		}
 		return $arr;
 	}
+
+	function insert($tbl,$arr)
+	{
+		$column_arr=array_keys($arr);
+		$column=implode(",",$column_arr);
+		
+		$values_arr=array_values($arr);
+		$values=implode("','",$values_arr); 
+		
+		$sel="insert into $tbl ($column) value ('$values')";  // query
+		$run=$this->conn->query($sel);  // query run on db
+		return $run;
+	}
+	
+
 	
 }
 $obj=new model
